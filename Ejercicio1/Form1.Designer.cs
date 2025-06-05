d789e1f89a8e04ed479165748e694d18c78749e7
Jossdel <josuedelgadil01@gmail.com>
2025-06-04 22:22:40 -0400

    ejercicio 3

--- Ejercicio1/Form1.Designer.cs
+++ Ejercicio1/Form1.Designer.cs
@@ -60,7 +60,7 @@
             this.label3.AutoSize = true;
             this.label3.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(15, 252);
+            this.label3.Location = new System.Drawing.Point(12, 251);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(515, 36);
             this.label3.TabIndex = 2;
