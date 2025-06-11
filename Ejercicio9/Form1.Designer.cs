2855738b6404bb4280d3c82550cf6421fc8d950a
Jossdel <josuedelgadil01@gmail.com>
2025-06-11 15:20:43 -0400

    Ejercicio 10

--- Ejercicio9/Form1.Designer.cs
+++ Ejercicio9/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Ejercicio8
+﻿namespace Ejercicio9
 {
     partial class Form1
     {
@@ -28,49 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(295, 150);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(156, 55);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(295, 337);
+            this.button1.Location = new System.Drawing.Point(214, 327);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(156, 53);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "send";
+            this.button1.Size = new System.Drawing.Size(286, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Enviar";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(99, 282);
+            this.label1.Location = new System.Drawing.Point(209, 247);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(570, 35);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "increments by a number each time you press";
+            this.label1.Size = new System.Drawing.Size(236, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Esperando entrada...";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(214, 148);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(286, 46);
+            this.textBox1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.NavajoWhite;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -80,9 +79,9 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
---
+++ Ejercicio9/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text;
+            if (int.TryParse(label1.Text as string , out _) || double.TryParse(label1.Text as string, out _))
+            {
+                label1.Text = " El digito que ingreso es un numero";
+            }
+             else if (String.IsNullOrEmpty(textBox1.Text)){
+                label1.Text = " No se permite este campo vacio";
+            }
+            else
+            {
+                label1.Text = " lo siento lo que ingreso no es un numero";
+                
+
+            }
+
+        }
+    }
+}
