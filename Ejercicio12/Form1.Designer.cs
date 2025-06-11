d863efaf772aa86d3e87453e4be755aa5958052f
Jossdel <josuedelgadil01@gmail.com>
2025-06-11 15:49:06 -0400

    Arreglo al ejercicio 12

--- Ejercicio12/Form1.Designer.cs
+++ Ejercicio12/Form1.Designer.cs
@@ -28,13 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TexBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(173, 346);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(380, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TexBox1
+            // 
+            this.TexBox1.Location = new System.Drawing.Point(173, 171);
+            this.TexBox1.Multiline = true;
+            this.TexBox1.Name = "TexBox1";
+            this.TexBox1.Size = new System.Drawing.Size(380, 46);
+            this.TexBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(168, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(370, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Convertir de Celsius a Fahrenheit";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(181, 314);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TexBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TexBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Ejercicio12/Form1.cs
+++ Ejercicio12/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Ejercicio12
 {
@@ -15,6 +16,27 @@ namespace Ejercicio12
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(TexBox1.Text, out double celsius))
+            {
+                double fahrenheit = (celsius * 9 / 5) + 32;
+                label2.Text = "Fahrenheit: " + fahrenheit;
+                ;
+               
+            }
+            else
+            {
+                label2.Text = " Ingrese un numero que sea valido ";
+               
+            }
         }
     }
 }
