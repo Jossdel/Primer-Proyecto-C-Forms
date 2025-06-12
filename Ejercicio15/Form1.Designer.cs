01ffc3cf67a4292373beba2b834219a5cc945019
Jossdel <josuedelgadil01@gmail.com>
2025-06-11 20:35:36 -0400

    Ejercicio 15

--- Ejercicio15/Form1.Designer.cs
+++ Ejercicio15/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Ejercicio13
+﻿namespace Ejercicio15
 {
     partial class Form1
     {
@@ -30,49 +30,49 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(149, 329);
+            this.button1.Location = new System.Drawing.Point(62, 245);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(139, 57);
+            this.button1.Size = new System.Drawing.Size(228, 74);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Iniciar";
+            this.button1.Text = "Abrir";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(463, 332);
+            this.button2.Location = new System.Drawing.Point(470, 245);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(147, 54);
+            this.button2.Size = new System.Drawing.Size(206, 74);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Detener";
+            this.button2.Text = "Guardar";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // progressBar1
+            // textBox1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(121, 138);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(518, 49);
-            this.progressBar1.TabIndex = 2;
-            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            this.textBox1.Location = new System.Drawing.Point(62, 93);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(616, 93);
+            this.textBox1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Gainsboro;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -80,7 +80,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
---
+++ Ejercicio15/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Ejercicio15
+{
+    public partial class Form1 : Form
+    {
+        private string rutaArchivoActual = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Solo esta definición debe existir
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Archivos de texto (*.txt)|*.txt|Todos los archivos (*.*)|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = File.ReadAllText(ofd.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(rutaArchivoActual))
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Archivos de texto (*.txt)|*.txt|Todos los archivos (*.*)|*.*";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    rutaArchivoActual = saveFileDialog.FileName;
+                }
+                else
+                {
+                    return; // Cancelado
+                }
+            }
+
+            File.WriteAllText(rutaArchivoActual, textBox1.Text);
+            MessageBox.Show("Archivo guardado correctamente.");
+        }
+
+
+    }
+}
