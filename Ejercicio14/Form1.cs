4e650453159466f518a85552cd3e46cfe574980e
Jossdel <josuedelgadil01@gmail.com>
2025-06-11 20:17:00 -0400

    Ejercicio 14

--- Ejercicio14/Form1.cs
+++ Ejercicio14/Form1.cs
@@ -16,5 +16,12 @@ namespace Ejercicio14
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tabPage1.Controls.Add(new Label { Text = "Esta es la Pestaña 1", Dock = DockStyle.Fill, TextAlign = System.Drawing.ContentAlignment.MiddleCenter });
+            tabPage2.Controls.Add(new Label { Text = "Esta es la pestaña Pestaña 2", Dock = DockStyle.Fill, TextAlign = System.Drawing.ContentAlignment.MiddleCenter });
+
+        }
     }
 }
